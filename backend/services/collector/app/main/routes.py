9d561841d06cd0144fc8da7f46cb63afa9015c15
HenryKim12 <henrykim966@gmail.com>
2024-07-05 20:27:25 -0700

    moved data processing logic to services.py

--- backend/services/collector/app/main/routes.py
+++ backend/services/collector/app/main/routes.py
@@ -5,6 +5,7 @@ from ..models import Restaurant
 import requests
 import os
 from dotenv import load_dotenv
+from . import services
 
 load_dotenv()
 
@@ -16,65 +17,9 @@ def index():
 def about():
     return "This is the about page."
 
-@main.route("/fetch")
-def fetchRestaurants():
-    headers = {
-        "accept": "application/json", 
-        "Authorization": f"Bearer {os.getenv("API_KEY")}",
-    }
-    params = {
-        "location": "Vancouver",
-        "term": "restaurants",
-        "limit": 50,
-    }
-
-    daysOfWeek = {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday"
-    }
-
-    res = requests.get("https://api.yelp.com/v3/businesses/search", headers=headers, params=params)
-    restaurants = res["businesses"]
-    for restaurant in restaurants:
-        name = restaurant["name"]
-        yelp_business_id = restaurant["id"]
-        display_address = restaurant["location"]["display_address"]
-        location = ""
-        for address in display_address:
-            location += address
-        cuisine = restaurant["id"] #categories??
-        priceRange = restaurant["price"]
-        rating = restaurant["rating"]
-        weeklyHours = restaurant["business_hours"]["open"]
-        businessHours = ""
-        for i in range(7):
-            dailyHours = weeklyHours[i]
-            businessHours += daysOfWeek[dailyHours["day"]]
-            businessHours += " from "
-            businessHours += dailyHours["start"]
-            businessHours += " to "
-            businessHours += dailyHours["end"]
-
-    # Iterate through all of Vancouver restaurants
-    # params = {
-    #     "location": "Vancouver",
-    #     "term": "restaurants",
-    #     "limit": 50,
-    #     "offset": 1
-    # }
-    # total = None
-    # params["offset"] += 50
-    # while (not total or params["offset"] <= total):
-    #     res = requests.get("https://api.yelp.com/v3/businesses/search", headers=headers, params=params)
-    #     params["offset"] += 50
-    #     if not total:
-    #         total = res.json()["total"]
-
+@main.route("/collect")
+def collect():
+    services.fetchRestaurants()
     return "Successfully fetched Vancouver restaurants"
 
 @main.route("/add_restaurant")
---
+++ backend/services/collector/app/main/services.py
@@ -0,0 +1,63 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def fetchRestaurants():
+    headers = {
+        "accept": "application/json", 
+        "Authorization": f"Bearer {os.getenv("API_KEY")}",
+    }
+    params = {
+        "location": "Vancouver",
+        "term": "restaurants",
+        "limit": 50,
+    }
+
+    daysOfWeek = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday"
+    }
+
+    res = requests.get("https://api.yelp.com/v3/businesses/search", headers=headers, params=params)
+    restaurants = res["businesses"]
+    for restaurant in restaurants:
+        name = restaurant["name"]
+        yelp_business_id = restaurant["id"]
+        display_address = restaurant["location"]["display_address"]
+        location = ""
+        for address in display_address:
+            location += address
+        cuisine = restaurant["id"] #categories??
+        priceRange = restaurant["price"]
+        rating = restaurant["rating"]
+        weeklyHours = restaurant["business_hours"]["open"]
+        businessHours = ""
+        for i in range(7):
+            dailyHours = weeklyHours[i]
+            businessHours += daysOfWeek[dailyHours["day"]]
+            businessHours += " from "
+            businessHours += dailyHours["start"]
+            businessHours += " to "
+            businessHours += dailyHours["end"]
+
+    # Iterate through all of Vancouver restaurants
+    # params = {
+    #     "location": "Vancouver",
+    #     "term": "restaurants",
+    #     "limit": 50,
+    #     "offset": 1
+    # }
+    # total = None
+    # params["offset"] += 50
+    # while (not total or params["offset"] <= total):
+    #     res = requests.get("https://api.yelp.com/v3/businesses/search", headers=headers, params=params)
+    #     params["offset"] += 50
+    #     if not total:
+    #         total = res.json()["total"]
