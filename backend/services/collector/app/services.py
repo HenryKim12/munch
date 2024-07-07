ff94bb64ae9f2d059055dcdde8744eae090a7eb0
HenryKim12 <henrykim966@gmail.com>
2024-07-06 22:21:17 -0700

    scrape categories from yelp url using bs4

--- backend/services/collector/app/services.py
+++ backend/services/collector/app/services.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
@@ -31,29 +32,44 @@ def fetchRestaurants():
         "limit": 50,
     }
 
-    response = requests.get(YELP_API_URL, headers=headers, params=params)
-    restaurants = response.json()["businesses"]
-    for restaurant in restaurants:
-        yelp_business_id = restaurant["id"]
-        name = restaurant["name"]
-        phone_number = restaurant["display_phone"]
-        menu_url = restaurant["attributes"]["menu_url"]
-        price = restaurant["price"]
-        rating = restaurant["rating"]
+    # response = requests.get(YELP_API_URL, headers=headers, params=params)
+    # restaurants = response.json()["businesses"]
+    # for restaurant in restaurants:
+    #     yelp_business_id = restaurant["id"]
+    #     name = restaurant["name"]
+    #     phone_number = restaurant["display_phone"]
+    #     menu_url = restaurant["attributes"]["menu_url"]
+    #     price = restaurant["price"]
+    #     rating = restaurant["rating"]
 
-        address = ""
-        display_address = restaurant["location"]["display_address"]
-        for val in display_address:
-            address += val
+    #     address = ""
+    #     display_address = restaurant["location"]["display_address"]
+    #     for val in display_address:
+    #         address += val
 
-        open_hours = restaurant["business_hours"]["open"]
-        business_hours = {k: [] for k in range(7)}
-        for val in open_hours:
-            daily_hours = f"{val["start"]} to {val["end"]}"
-            business_hours[val["day"]].append(daily_hours)
+    #     open_hours = restaurant["business_hours"]["open"]
+    #     business_hours = {k: [] for k in range(7)}
+    #     for val in open_hours:
+    #         daily_hours = f"{val["start"]} to {val["end"]}"
+    #         business_hours[val["day"]].append(daily_hours)
 
-        url = restaurant["url"]
-        # categories (people also searched for section) + popular dishes grabbed from scraping page
+    #     url = restaurant["url"]
+    #     # categories (people also searched for section) + popular dishes grabbed from scraping page
 
-    return response.json()
+    # return response.json()
+    return scrape("hi")
+
+def scrape(url: str) -> list[str]: 
+    response = requests.get("https://www.yelp.com/biz/the-flying-pig-vancouver-5?adjust_creative=SuhzlSss_Ymp7bpjhwEWSA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SuhzlSss_Ymp7bpjhwEWSA")
+    soup = BeautifulSoup(response.text, "html.parser")
+    main = soup.find_all("main", {"id": "main-content"})[0]
+    section = main.find_all("section", {"aria-label": "People also searched for"})[0]
+    categories = section.find_all("a")
+
+    result = []
+    for category in categories:
+        value = category.find_all("p")[0]
+        result.append(value.decode_contents())
+
+    return result
         
