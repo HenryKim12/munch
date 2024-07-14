1e60bb0cde3f3e3c20e95551d7b8f296d2be73a9
HenryKim12 <henrykim966@gmail.com>
2024-07-13 21:19:17 -0700

    attempt user-restaurant relationship

--- backend/services/user/app/models.py
+++ backend/services/user/app/models.py
@@ -5,15 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# user_restaurant = db.Table('user_restaurant', 
+#     db.Column('user_id', db.Integer, db.ForeignKey(f'{os.getenv("USER_SCHEMA")}.user.id')),
+#     db.Column('restaurant_id', db.Integer, db.ForeignKey(f'{os.getenv("RESTAURANT_SCHEMA")}.restaurant.id'))
+# )
+
 class User(db.Model):
     __tablename__ = 'user'
-    __table_args__ = {'schema': os.getenv("SCHEMA")}
+    __table_args__ = {'schema': os.getenv("USER_SCHEMA")}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    # restaurants = db.relationship('Restaurant', secondary=user_restaurant, backref='users')
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.username}>'
+    
--- backend/services/user/app/routes.py
+++ backend/services/user/app/routes.py
@@ -11,6 +11,19 @@ main = Blueprint('main', __name__)
 @main.route('/users', methods=["GET"])
 def get():
     pass
+
 @main.route('/users/<int:id>', methods=["GET"])
 def getByID(id):
+    pass
+
+@main.route('/users/<int:id>', methods=["DELETE"])
+def delete(id):
+    pass
+
+@main.route('/users', methods=["POST"])
+def post():
+    pass
+
+@main.route('/users', methods=["PUT"])
+def update():
     pass
