82837442f3d628ceb25420eb64d3c9dfb5578a9c
HenryKim12 <henrykim966@gmail.com>
2024-07-14 19:16:24 -0700

    add UserRestaurant model

--- backend/services/user/app/models.py
+++ backend/services/user/app/models.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# user_restaurant = db.Table('user_restaurant', 
-#     db.Column('user_id', db.Integer, db.ForeignKey(f'{os.getenv("USER_SCHEMA")}.user.id')),
-#     db.Column('restaurant_id', db.Integer, db.ForeignKey(f'{os.getenv("RESTAURANT_SCHEMA")}.restaurant.id'))
-# )
-
 class User(db.Model):
     __tablename__ = 'user'
     __table_args__ = {'schema': os.getenv("USER_SCHEMA")}
@@ -17,10 +12,18 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    # restaurants = db.relationship('Restaurant', secondary=user_restaurant, backref='users')
+    restaurants = db.relationship('UserRestaurant', back_populates='user')
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
     
+
+class UserRestaurant(db.Model):
+    __tablename__ = 'user_restaurant'
+    __table_args__ = {'schema': os.getenv("USER_SCHEMA")}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(f'{os.getenv("USER_SCHEMA")}.user.id'), nullable=False)  
+    restaurant_id = db.Column(db.Integer, nullable=False)
+    user = db.relationship('User', back_populates='restaurants')  
