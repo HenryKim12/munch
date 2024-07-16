5cb89f9ac6b8c3d007ffd5f16df9d7a38a66bd40
HenryKim12 <henrykim966@gmail.com>
2024-07-16 16:22:30 -0700

    updated user models to use usr schema and table

--- backend/services/user/app/models.py
+++ backend/services/user/app/models.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'usr'
     __table_args__ = {'schema': os.getenv("USER_SCHEMA")}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    restaurants = db.relationship('UserRestaurant', back_populates='user')
+    restaurants = db.relationship('UserRestaurant', back_populates='user', cascade="all, delete-orphan")
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False, unique=False)
 
@@ -27,12 +27,11 @@ class User(db.Model):
             "password": self.password,
             "restaurants": self.restaurants
         }
-    
 
 class UserRestaurant(db.Model):
-    __tablename__ = 'user_restaurant'
+    __tablename__ = 'user_restaurants'
     __table_args__ = {'schema': os.getenv("USER_SCHEMA")}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(f'{os.getenv("USER_SCHEMA")}.user.id'), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey(f'{os.getenv("USER_SCHEMA")}.usr.id'), nullable=False)  
     restaurant_id = db.Column(db.Integer, nullable=False)
     user = db.relationship('User', back_populates='restaurants')  
